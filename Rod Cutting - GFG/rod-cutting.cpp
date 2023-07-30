67a36348299167496780726b63dd309c2fb2104d
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-07-30 22:21:49 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,35 +10,54 @@ using namespace std;
 
 class Solution{
   public:
-  int get_maxvalue(int target,int value[],vector<vector<int>>&dp){
-    int n=target;
-    //   // base case 
+//   int get_maxvalue(int idx,int target,int value[],vector<vector<int>>&dp){
+//       // base case 
+      
+//       if(idx==0){
+//           return value[0]*target;
+//       }
+//     // ----- > or ---> both are same here we are not stopping at idx at 0
+//     // if(target==0|| idx<0)return 0;
+//          // take or no take case
+//       if(dp[idx][target]!=-1)return dp[idx][target];
+//       int take=0;
+//       if(target-(idx+1)>=0){
+//           take=get_maxvalue(idx,target-(idx+1),value,dp)+value[idx];
+//       }
+//       int notake=get_maxvalue(idx-1,target,value,dp);
+//       return dp[idx][target]=max(take,notake);
+//   }
+  int get_maxvalueTabulated(int target,int price[]){
+      // base case 
+      int n=target;
     //   if(idx==0){
-    //       return target*value[0];
+    //       return value[0]*target;
     //   }
-    
-    for(int tar=0;tar<=target;++tar){
-        dp[0][tar]=tar*value[0];
-    }
-   
-  for(int idx=1;idx<n;++idx){
-      for(int tar=0;tar<=target;++tar){
-        int take=-1e9;
-        int weight=idx+1;
-          if(tar-weight>=0){
-              take=dp[idx][tar-weight]+value[idx];
-          }
-      int notake=dp[idx-1][tar];
-        dp[idx][tar]=max(take,notake);
+    // ----- > or ---> both are same here we are not stopping at idx at 0
+    // if(target==0|| idx<0)return 0;
+    vector<vector<int>>dp(n,vector<int>(n+1,0));
+     for(int tar=0;tar<=target;++tar){
+         dp[0][tar]=price[0]*tar;
+     }
+     
+     for(int idx=1;idx<n;++idx){
+         for(int tar=0;tar<=target;++tar){
+               // take or no take case
+              int take=0;
+              if(tar-(idx+1)>=0){
+                  take=dp[idx][tar-(idx+1)]+price[idx];
+              }
+               int notake=dp[idx-1][tar];
+               
+               dp[idx][tar]=max(take,notake);
+        }
       }
-  }
-   return dp[n-1][n];
+      return dp[n-1][n];
+     
   }
     int cutRod(int price[], int n) {
         //code here
-        vector<vector<int>>dp(n,vector<int>(n+1,0));
-        
-        return get_maxvalue(n,price,dp);
+        return get_maxvalueTabulated(n,price);
     }
 };
 
