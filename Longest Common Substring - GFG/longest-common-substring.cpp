392954290e5165d8e07555abfd8f97f34a3df0d4
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-08-03 15:43:54 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -5,33 +5,50 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    
+    int Optimised(string S1, string S2, int n, int m){
+        
+       vector<int>dp(m+1,0);
+       
+        int mx=-1e9;
+       
+        for(int i=1;i<=n;++i){
+             vector<int>curr(m+1,0);
+            
+            for(int j=1;j<=m;++j){
+                if(S1[i-1]==S2[j-1]){
+                    curr[j]=1+dp[j-1];
+                }else
+                curr[j]=0;
+                mx=max(mx,curr[j]);
+            }
+            dp=curr;
+        }
+        return mx;
+    }
     int longestCommonSubstr (string S1, string S2, int n, int m)
     {
         // your code here
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;++i){
-            for(int j=1;j<=m;++j){
-                if(S1[i-1]==S2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else
-                dp[i][j]=0;
-            }
-        }
-        // TO UNDERSTAND SEE THIS 
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        //         int mx=-1e9;
+        // for(int i=1;i<=n;++i){
+        //     for(int j=1;j<=m;++j){
+        //         if(S1[i-1]==S2[j-1]){
+        //             dp[i][j]=1+dp[i-1][j-1];
+        //         }else
+        //         dp[i][j]=0;
+        //      mx=max(mx,dp[i][j]);
+        //     }
+        // }
+        // // TO UNDERSTAND SEE THIS 
         // for(auto in:dp){
         //     for(auto ele:in){
         //         cout<<ele<<" ";
         //     }cout<<endl;
         // }
-        int mx=-1e9;
-            for(auto in:dp){
-            for(auto ele:in){
-                mx=max(mx,ele);
-            }
-        }
+
+        return Optimised(S1, S2,n, m);
         
-        return mx;
+
     }
 };
 
